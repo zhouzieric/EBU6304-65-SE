daebfc04fbb3ea513fc91d62b4a2fe598686af2c
zhouzieric <55612911+zhouzieric@users.noreply.github.com>
2021-05-10 15:43:48 +0800

    Delete pages directory

--- pages/CustomLoginJp.java
+++
@@ -1,7 +0,0 @@
-package pages;
-//用户登录页面
-public class CustomLoginJp extends CustoTraLoginJp {
-    public CustomLoginJp(){
-        super();
-    }
-}
--- pages/TraLoginJp.java
+++
@@ -1,8 +0,0 @@
-package pages;
-
-//教练登录页面
-public class TraLoginJp extends CustoTraLoginJp {
-    public TraLoginJp(){
-        super();
-    }
-}
